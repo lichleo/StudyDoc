fb2baf78cbc3aa825c9bbf62c46f049b8c3147a4
Lian, Hai Tao <haitao.lian@siemens.com>
2024-07-24 17:00:37 +0800

    new

--- docs/conf.py
+++ docs/conf.py
@@ -2,31 +2,19 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import sphinx_rtd_theme
-import recommonmark, os, sys
-from recommonmark.parser import CommonMarkParser
-from recommonmark.transform import AutoStructify
-
-HERE = os.path.dirname(__file__)
-sys.path.insert(0, os.path.abspath(r'../../src'))
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-source_suffix = ['.rst', '.md']
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PYMUD 帮助文档'
-copyright = '2023-2024, crapex@crapex.cc'
-author = 'crapex'
-release = '0.19.3post1'
+project = 'python学习笔记'
+copyright = '2024, lichleo'
+author = 'lichleo'
+release = 'v1.00'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark', 'sphinx_markdown_tables', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
+extensions = ['recommonmark','sphinx_markdown_tables']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -39,3 +27,4 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
