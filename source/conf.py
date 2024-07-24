c9a79bc4ff565bb805e7347bac18e16f0ef1a6d0
Kokomi <lian_haitao@163.com>
2024-07-24 16:32:19 +0800

    new

---
+++ source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sphinx_rtd_theme
+import recommonmark, os, sys
+from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
+
+HERE = os.path.dirname(__file__)
+sys.path.insert(0, os.path.abspath(r'../../src'))
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PYMUD 帮助文档'
+copyright = '2023-2024, crapex@crapex.cc'
+author = 'crapex'
+release = '0.19.3post1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark', 'sphinx_markdown_tables', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# sphinx_bootstrap_theme  sphinx_rtd_theme sphinx_nefertiti
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ source/index.rst
@@ -0,0 +1,39 @@
+.. python学习笔记 documentation master file, created by
+   sphinx-quickstart on Sun Feb  4 09:39:35 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+学习笔记
+==========================================
+
+
+写在最前面的话
+^^^^^^^^^^^^^^^
+
+好好学习，天天向上！
+
+
+主要内容
+^^^^^^^^^
+
+- 学习python高级特性
+- python有用的库
+
+
+.. toctree::
+   :maxdepth: 3
+   :caption: 目录
+
+   正则表达式
+   hotkeys
+   scripts
+   plugins
+   updatehistory
+   
+
+索引与表
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
